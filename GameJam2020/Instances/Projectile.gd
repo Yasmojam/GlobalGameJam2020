15c236247ee7ef39946fd6d2b237f2fe8347eb6f
Iain Cattermole <iain_c@live.co.uk>
2020-02-01 16:20:17 +0000

    bullet lifetimes

--- GameJam2020/Instances/Projectile.gd
+++ GameJam2020/Instances/Projectile.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
 export var speed = 400
+export var MAX_LIFE = 3  # seconds
+
+var time_alive = 0
 
 var control = false
 var bullet_id
 
-func _ready():
-	print('shot')
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if control:
+	time_alive += delta
+	if time_alive > MAX_LIFE:
+		rpc("delete", bullet_id)
+		delete(bullet_id)
+	
+	if control:  # shot by current player
 		position.y -= speed * delta
 		rpc_unreliable("move_projectile", bullet_id, position)
+		
+		# TODO: check for collision
+
+
+remote func delete(id):
+	if id == bullet_id:
+		queue_free()
+
 
 remote func move_projectile(id, pos):
 	if id == bullet_id:
