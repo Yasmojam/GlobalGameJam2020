832a615fcb7b9d638dcccb2ee7f934c831819542
Iain Cattermole <iain_c@live.co.uk>
2020-02-01 14:26:38 +0000

    networking

--- GameJam2020/Instances/Player.gd
+++ GameJam2020/Instances/Player.gd
@@ -7,12 +7,21 @@ export var grav = 0.09375
 var velocity = Vector2()
 var inAir = false
 
+# Networking
+var control = false
+var player_id = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if control == true:
+		_handle_movement(delta)
+
+func _handle_movement(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -35,4 +44,12 @@ func _process(delta):
 		velocity.y += grav
 	$AnimatedSprite.play()
 	position += velocity * speed * delta
+	rpc_unreliable("move_player", position, player_id)
+	
+
+remote func move_player(pos, player_id):
+	var root = get_parent()
+	var player = root.get_node(str(player_id))
+	player.position = pos
+	
 	
---
+++ GameJam2020/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('calling start server')
+	$network.start_server()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
