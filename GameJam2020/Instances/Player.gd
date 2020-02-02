c2679b58dc1c64c9e1e95535e0f0cf47f4948112
Iain Cattermole <iain_c@live.co.uk>
2020-02-02 13:03:39 +0000

    remove debugging

--- GameJam2020/Instances/Player.gd
+++ GameJam2020/Instances/Player.gd
@@ -49,11 +49,9 @@ func _handle_movement(delta):
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -1 * jumpInitSpeed
 	if velocity.x != 0:
-		#$AnimatedSprite.animation = "Walking"
-		pass
+		$AnimatedSprite.animation = "Walking"
 	else:
-		pass
-		#$AnimatedSprite.animation = "Standing"
+		$AnimatedSprite.animation = "Standing"
 	$AnimatedSprite.play()
 	
 	move_and_slide(velocity*speed, Vector2(0, -1), true)
