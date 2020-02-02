b008b53b813cb510095eb83a34ddae8cad5ccb81
Iain Cattermole <iain_c@live.co.uk>
2020-02-02 14:27:33 +0000

    fix

--- GameJam2020/Instances/BadScrap.gd
+++ GameJam2020/Instances/BadScrap.gd
@@ -39,8 +39,9 @@ func _on_Area2D_body_entered(body):
 		var good_scrap = good_scrap_scene.instance()
 		good_scrap.position = Vector2(position.x, position.y)
 		good_scrap.control = true
-		good_scrap.scrap_id = scrap_id
+		good_scrap.scrap_id = scrap_id + "good"
 		good_scrap.manager = manager
+		good_scrap.set_name(scrap_id + "good")
 		get_parent().add_child(good_scrap)
 		queue_free()
 	if (body.name == "GroundTileMap"):
